26d81a6c04f130338e03f9f847b655b16d380f87
Mayas Haddad <mayas_91@hotmail.com>
2015-10-15 17:09:09 +0200

    corrected the test environment, now alrght

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -8,15 +8,12 @@ module.exports = function(config) {
     basePath: '',
 
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
-
+ 
     // list of files / patterns to load in the browser
     files: [
+      '*.js'
     ],
-
 
     // list of files to exclude
     exclude: [
---
+++ tests/test.js
@@ -0,0 +1,9 @@
+describe("an example of tests", function() { 
+	it("4 is expected to equal 4", function() {
+		expect(4).toEqual(4);
+	});
+
+	it("5 is not expected to equal 4", function() {
+		expect(5).toEqual(4);
+	});
+});
