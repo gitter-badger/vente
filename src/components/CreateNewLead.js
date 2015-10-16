d7c04c7e0630741a4c034213504c3ce6b33c61b2
Mayas Haddad <mayas_91@hotmail.com>
2015-10-16 15:01:42 +0200

    added the first component

---
+++ src/components/CreateNewLead.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+/**
+* This is the component which wraps the login buttons
+* @class LoginElement
+* @classdesc This class wraps the different login buttons and handles their actions (facebook, google, github or simple sign up). 
+* @author Mayas Haddad
+*/
+export default class CreateNewLead extends React.Component {
+    
+    /**
+    **  @name constructor
+    **/
+   constructor(props) {
+      super(props)
+    }
+   
+   /**
+    * renders the different login buttons
+    * @returns the JSX representing the login element.
+    */
+   render() {
+      return (
+       <div>
+          Hello World!
+       </div>
+      )
+    }
+}
