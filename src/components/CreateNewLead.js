2f06d0f5b8983fdde0901d777f82d2f76f4e7b14
Mayas Haddad <mayas_91@hotmail.com>
2015-10-16 18:24:23 +0200

    added i18n messages and the styles of the app

--- src/components/CreateNewLead.js
+++ src/components/CreateNewLead.js
@@ -1,29 +1,70 @@
 import React from 'react'
+import '../../lib/gridforms/gridforms.css'
+import '../styles/main.css'
+window.$ = window.jQuery = require('../../lib/jquery.js')
+import '../../lib/gridforms/gridforms.js'
+import Messages from '../constants/messages.json'
 
 /**
-* This is the component which wraps the login buttons
-* @class LoginElement
-* @classdesc This class wraps the different login buttons and handles their actions (facebook, google, github or simple sign up). 
+* This is the component which renders the form for creating a new lead
+* @class CreateNewLead
 * @author Mayas Haddad
 */
 export default class CreateNewLead extends React.Component {
     
-    /**
-    **  @name constructor
-    **/
-   constructor(props) {
-      super(props)
+  /**
+  **  @name constructor
+  **/
+  constructor() {
+    this.state = {
+      locale: 'fr-FR'
     }
+  }
    
-   /**
-    * renders the different login buttons
-    * @returns the JSX representing the login element.
-    */
-   render() {
-      return (
-       <div>
-          Hello World!
-       </div>
-      )
-    }
+  /**
+  * renders the JSX representing the add new lead form.
+  * @returns {jsx}
+  */
+  render() {
+    let vCreateNewLeadMessages = Messages[this.state.locale]
+    .createNewLeadFormLabels
+     
+    return (
+      <form className="grid-form">
+        <fieldset>
+            <legend style={{fontFamily: 'RobotoRegular'}}>{vCreateNewLeadMessages.formName}</legend>
+            <div data-row-span="2">
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.firstname}</label>
+                <input type="text"/>
+              </div>
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.lastname}</label>
+                <input type="text"/>
+              </div>
+            </div>
+            <div data-row-span="3">
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.email}</label>
+                <input type="text"/>
+              </div>
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.company}</label>
+                <input type="text"/>
+              </div>
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.address}</label>
+                <input type="text"/>
+              </div>
+            </div>
+            <div data-row-span="1">
+              <div data-field-span="1">
+                <label>{vCreateNewLeadMessages.complementaryNote}</label>
+                <textarea></textarea>
+              </div>
+            </div>
+        </fieldset>
+      </form>
+    )
+  }
 }
