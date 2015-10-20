ec22e5b856bf30ca556eaa8be360d4e1fed8bee3
Mayas Haddad <mayas_91@hotmail.com>
2015-10-20 20:27:43 +0200

    added the "add" button  to create new lead form

--- src/components/CreateNewLead.js
+++ src/components/CreateNewLead.js
@@ -4,6 +4,7 @@ import '../styles/main.css'
 window.$ = window.jQuery = require('../../lib/jquery.js')
 import '../../lib/gridforms/gridforms.js'
 import Messages from '../constants/messages.json'
+import Button from '../../lib/utilityComponents/Button.js'
 
 /**
 * This is the component which renders the form for creating a new lead
@@ -29,42 +30,47 @@ export default class CreateNewLead extends React.Component {
     let vCreateNewLeadMessages = Messages[this.state.locale]
     .createNewLeadFormLabels
      
+    let f = () => { console.log('click on button') }
+    
     return (
-      <form className="grid-form">
-        <fieldset>
-            <legend style={{fontFamily: 'RobotoRegular'}}>{vCreateNewLeadMessages.formName}</legend>
-            <div data-row-span="2">
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.firstname}</label>
-                <input type="text"/>
+      <div>
+        <form className="grid-form">
+          <fieldset>
+              <legend style={{fontFamily: 'RobotoRegular'}}>{vCreateNewLeadMessages.formName}</legend>
+              <div data-row-span="2">
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.firstname}</label>
+                  <input type="text"/>
+                </div>
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.lastname}</label>
+                  <input type="text"/>
+                </div>
               </div>
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.lastname}</label>
-                <input type="text"/>
+              <div data-row-span="3">
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.email}</label>
+                  <input type="text"/>
+                </div>
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.company}</label>
+                  <input type="text"/>
+                </div>
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.address}</label>
+                  <input type="text"/>
+                </div>
               </div>
-            </div>
-            <div data-row-span="3">
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.email}</label>
-                <input type="text"/>
+              <div data-row-span="1">
+                <div data-field-span="1">
+                  <label>{vCreateNewLeadMessages.complementaryNote}</label>
+                  <textarea></textarea>
+                </div>
               </div>
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.company}</label>
-                <input type="text"/>
-              </div>
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.address}</label>
-                <input type="text"/>
-              </div>
-            </div>
-            <div data-row-span="1">
-              <div data-field-span="1">
-                <label>{vCreateNewLeadMessages.complementaryNote}</label>
-                <textarea></textarea>
-              </div>
-            </div>
-        </fieldset>
-      </form>
+          </fieldset>
+        </form>
+        <Button clickCallback={f} label={'AJOUTER'} />
+      </div>
     )
   }
 }
