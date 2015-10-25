3b72a3b991f8ed92eb67e8a9faa2486d1b99a634
Mayas Haddad <mayas_91@hotmail.com>
2015-10-25 23:35:00 +0100

    now refreshing the data passed to the button to be submitted

--- src/components/LeadForm.js
+++ src/components/LeadForm.js
@@ -5,6 +5,7 @@ window.$ = window.jQuery = require('../../lib/jquery.js')
 import '../../lib/gridforms/gridforms.js'
 import Messages from '../constants/messages.json'
 import Button from '../utilityComponents/Button'
+import Immutable from 'immutable'
 
 /**
 * @class LeadForm
@@ -19,12 +20,29 @@ class LeadForm extends React.Component {
     
     super(props)
     this.props = props
-
+    this.handleChange = this.handleChange.bind(this)
+    
     this.state = {
       locale: 'fr-FR'
     }
   }
-   
+  
+  handleChange() {
+    let vOldState = Immutable.Map(this.state)
+    let vNewState = vOldState.set(
+      'lead', 
+      {
+            firstname: this.refs.firstname.value,
+            lastname: this.refs.lastname.value,
+            email: this.refs.email.value,
+            company: this.refs.company.value,
+            address: this.refs.address.value,
+            sideNote: this.refs.sideNote.value
+      }
+    )
+    this.setState(vNewState.toObject())
+  }
+  
   /**
   * renders the JSX representing the add new lead form.
   * @returns {jsx}
@@ -32,7 +50,7 @@ class LeadForm extends React.Component {
   render() {
     let vLeadFormLabels = Messages[this.state.locale]
     .leadFormLabels
-     
+    
     return (
       <form className="grid-form">
         <fieldset>
@@ -40,35 +58,35 @@ class LeadForm extends React.Component {
             <div data-row-span="2">
               <div data-field-span="1">
                 <label>{vLeadFormLabels.firstname}</label>
-                <input type="text" defaultValue={this.props.firstname} />
+                <input type="text" ref="firstname" onChange={this.handleChange} defaultValue={this.props.firstname} />
               </div>
               <div data-field-span="1">
                 <label>{vLeadFormLabels.lastname}</label>
-                <input type="text" defaultValue={this.props.lastname} />
+                <input type="text" ref="lastname" onChange={this.handleChange} defaultValue={this.props.lastname} />
               </div>
             </div>
             <div data-row-span="3">
               <div data-field-span="1">
                 <label>{vLeadFormLabels.email}</label>
-                <input type="text" defaultValue={this.props.email} />
+                <input type="text" ref="email" onChange={this.handleChange} defaultValue={this.props.email} />
               </div>
               <div data-field-span="1">
                 <label>{vLeadFormLabels.company}</label>
-                <input type="text" defaultValue={this.props.company} />
+                <input type="text" ref="company" onChange={this.handleChange} defaultValue={this.props.company} />
               </div>
               <div data-field-span="1">
                 <label>{vLeadFormLabels.address}</label>
-                <input type="text" defaultValue={this.props.address} />
+                <input type="text" ref="address" onChange={this.handleChange} defaultValue={this.props.address} />
               </div>
             </div>
             <div data-row-span="1">
               <div data-field-span="1">
                 <label>{vLeadFormLabels.complementaryNote}</label>
-                <textarea style={{height: 10 + 'em'}} defaultValue={this.props.complementaryNote}></textarea>
+                <textarea style={{height: 10 + 'em'}} onChange={this.handleChange} ref="sideNote" defaultValue={this.props.complementaryNote}></textarea>
               </div>
             </div>
         </fieldset>
-        <Button label={this.props.submitButtonLabel} />
+        <Button label={this.props.submitButtonLabel} data={this.state.lead} clickCallback={this.props.clickCallback} />
       </form>
     )
   }
@@ -83,7 +101,8 @@ React.statics = {
     email: React.PropTypes.string,
     company: React.PropTypes.string,
     address: React.PropTypes.string,
-    complementaryNote: React.PropTypes.string
+    complementaryNote: React.PropTypes.string,
+    clickCallback: React.PropTypes.func.isRequired
   },
   
   defaultProps: {
