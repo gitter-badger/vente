4330f3ef065a862ab85d107127c411de3fd5de34
Mayas Haddad <mayas_91@hotmail.com>
2015-10-30 23:32:12 +0100

    added the test command

--- src/common/DataCheck.js
+++ src/common/DataCheck.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export default class DataCheck {
-	static checkValidEmail(pEmail) {
 	
+	static checkValidEmail(pEmail) {	
+		let re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+        return re.test(pEmail)
 	}
 }
--- test/DataCheck.test.js
+++ test/DataCheck.test.js
@@ -1,14 +1,30 @@
-var DataCheck = require('../src/common/DataCheck.js');
-var Assert = require('assert');
-
 var DataCheckTest = (function() {
-
-	var testEmailWellFormed = function() {
-		Assert.equal(3, 4);
+	
+	var DataCheck = require('../src/common/DataCheck.js');
+	var Assert = require('assert');
+	
+	var testCheckValidEmail = function() {
+		
+		console.log('test for a valid email string')
+		Assert(DataCheck.checkValidEmail('email@example.com'));
+		
+		console.log('test for an ivalid email string: email@.com')
+		Assert(!DataCheck.checkValidEmail('email@.com'));
+		
+		console.log('test for an ivalid email string: @example.com')
+		Assert(!DataCheck.checkValidEmail('@example.com'));
+		
+		console.log('test for an ivalid email string: 34example')
+		Assert(!DataCheck.checkValidEmail('34example'));
+		
+		console.log('test for an ivalid email string: \'\'')
+		Assert(!DataCheck.checkValidEmail(''));
+		
+		console.log('testCheckValidEmail: TEST OK')
 	}
 
 	var run = function() {
-		testEmailWellFormed();
+		testCheckValidEmail();
 	}
 
 	return {
