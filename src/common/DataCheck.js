c94b189765297872e95d74f9f65fe5b9ca631c66
Mayas Haddad <mayas_91@hotmail.com>
2015-10-31 11:52:05 +0100

    added a data check function (isNumeric)

--- src/common/DataCheck.js
+++ src/common/DataCheck.js
@@ -6,4 +6,8 @@ export default class DataCheck {
 		let re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
         return re.test(pEmail)
 	}
+	
+	static isNumeric(pField) {
+		return  (!pField ? false : !isNaN(pField))
+	}
 }
--- test/DataCheck.test.js
+++ test/DataCheck.test.js
@@ -22,9 +22,31 @@ var DataCheckTest = (function() {
 		
 		console.log('testCheckValidEmail: TEST OK')
 	}
-
+	
+	var testIsNumeric = function() {
+		
+		console.log('test for a invalid number: alphabet')
+		Assert(!DataCheck.isNumeric('Not Numeric'))
+		
+		console.log('test for a invalid number: number followed by alphabet')
+		Assert(!DataCheck.isNumeric('23Not Numeric'))
+		
+		console.log('test for a invalid number: alphabet followed by number')
+		Assert(!DataCheck.isNumeric('v23'))
+		
+		console.log('test for a invalid number: empty string')
+		Assert(!DataCheck.isNumeric(''))
+		
+		console.log('test for a valid number: decimal point')
+		Assert(DataCheck.isNumeric('23.3'))
+		
+		console.log('test for a valid number: plain integer')
+		Assert(DataCheck.isNumeric('233'))
+	}
+	
 	var run = function() {
 		testCheckValidEmail();
+		testIsNumeric()
 	}
 
 	return {
