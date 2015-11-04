55bcb4f6ca5f91514fbbbe2a0ab520e5075981cc
Mayas Haddad <mayas_91@hotmail.com>
2015-11-04 15:18:48 +0100

    decommenterd the alert store call todo: implement the alert mecanism

--- src/actions/LeadActions.js
+++ src/actions/LeadActions.js
@@ -3,21 +3,23 @@ import Config from '../constants/config.json'
 
 export default class LeadActions {
 	constructor() {	
+        this.mAjax = new Ajax()
+        this.doAddLead = this.doAddLead.bind(this)
 	}
 	
 	doAddLead(pNewLead) {
-		var vAjax = new Ajax()
-		vAjax.post(Config.persistLeadServerURL, 'application/json;charset=UTF-8', pNewLead, 
+		this.mAjax.post(Config.persistLeadServerURL, 'application/json;charset=UTF-8', pNewLead, 
         function(response) {
-            // AlertsStore.addNewAlertMessage({
-            //             nature: Natures.SUCCESS,
-            //             content: "Great! The to-read-list has been updated!"
-            // });
+            AlertsStore.addNewAlertMessage({
+                        nature: Natures.SUCCESS,
+                        content: 'Great! The to-read-list has been updated!'
+            });
         }, function(e) { 
-            // AlertsStore.addNewAlertMessage({
-            //     nature: Natures.FAILURE,
-            //     content: "Woops :( couldn't update to-read-list"
-            // });
+            console.log(e)
+            AlertsStore.addNewAlertMessage({
+                nature: Natures.FAILURE,
+                content: 'Woops :( couldn\'t update to-read-list'
+            });
         })
 	}
 }
