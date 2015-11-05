3e0f628f374432543d348e8f385f9f31829e97d1
Mayas Haddad <mayas_91@hotmail.com>
2015-11-05 19:11:42 +0100

    added the index component

---
+++ src/components/Index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+/**
+* This is the component which renders the form for creating a new lead
+* @class CreateNewLead
+* @author Mayas Haddad
+*/
+export default class Index extends React.Component {
+    
+  /**
+  **  @name constructor
+  **/
+  constructor() {
+    this.state = {
+      locale: 'fr-FR'
+    }
+  }
+  
+  render() {
+	  return <div></div>
+  }
+}
