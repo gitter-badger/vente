fc580a0d52f9f9bb95592e1cfff9076aade422e5
Mayas Haddad <mayas_91@hotmail.com>
2015-11-12 16:37:55 +0100

    introducing SVG Icons

---
+++ src/utilityComponents/SVGIcon.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+/**
+* This is the component which renders the form for creating a new lead
+* @class Button
+* @author Mayas Haddad
+*/
+export default class SVGIcon extends React.Component {
+    
+  /**
+  **  @name constructor
+  **/
+  constructor(props) {
+    super(props)
+    this.props = props
+  }
+  
+  /**
+  * renders the JSX representing the add new lead form.
+  * @returns {jsx}
+  */
+  render() {
+    console.log(this.props.imageUrl)
+    const vMask = 'url(' + this.props.imageUrl + ') no-repeat ' + 50 + '% ' + 50 + '%',
+          vMaskSize = 'cover',
+          defaultStyle = {
+            WebkitMask: vMask,
+            mask: vMask,
+            WebkitMaskSize: vMaskSize,
+            maskSize: vMask
+          }
+
+    const vStyle = Object.assign(defaultStyle, this.props.customStyle)
+
+    return (<div style={ vStyle } ></div>)
+  }
+}
+
+SVGIcon.propTypes = {
+  imageUrl: React.PropTypes.string.isRequired,
+  customStyle: React.PropTypes.object.isRequired
+}
