2e5582602df1827b736d3bd76796c2e1b39d6ec3
Mayas Haddad <mayas_91@hotmail.com>
2015-11-13 16:56:04 +0100

    added the transparent effect on hover + cursor is now pointer

--- src/utilityComponents/MenuItem.js
+++ src/utilityComponents/MenuItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SVGIcon from './SVGIcon'
+import Immutable from 'immutable'
 
 /**
 * This is the component which renders the form for creating a new lead
@@ -14,28 +15,50 @@ export default class MenuItem extends React.Component {
   constructor(props) {
     super(props)
     this.props = props
+
+    this.goOpac = this.goOpac.bind(this)
+    this.goTransparent = this.goTransparent.bind(this)
+
+    const vMenuItemStyle = { 
+      fontFamily: 'RobotoRegular',
+      display: '-webkit-flex',
+      alignItems: 'center',
+      cursor: 'pointer'
+    }
+
+    this.state = { menuItemStyle : vMenuItemStyle }
   }
   
+  goOpac() {
+    let vOldMenuItemStyle = new Immutable.Map(this.state.menuItemStyle)
+
+    let vNewMenuItemStyle = vOldMenuItemStyle.set('opacity', 1)
+
+    this.setState({ menuItemStyle: vNewMenuItemStyle.toObject() })
+  }
+
+  goTransparent() {
+    let vOldMenuItemStyle = new Immutable.Map(this.state.menuItemStyle)
+
+    let vNewMenuItemStyle = vOldMenuItemStyle.set('opacity', 0.5)
+
+    this.setState({ menuItemStyle: vNewMenuItemStyle.toObject() })
+  }
+
   /**
   * renders the JSX representing the add new lead form.
   * @returns {jsx}
   */
   render() {
 
-    const vMenuItemStyle = { 
-      fontFamily: 'RobotoRegular',
-      display: '-webkit-flex',
-      alignItems: 'center'
-    },
-
-    vMenuItemIconStyle = {
+    const vMenuItemIconStyle = {
       width: 32 + 'px', 
       height: 32 + 'px', 
       backgroundColor: '#FFFFFF',
       display: 'inline-block'
     }
 
-    return <div style={ vMenuItemStyle }>
+    return <div onMouseOver={this.goTransparent} onMouseOut={this.goOpac} style={ this.state.menuItemStyle }>
       <SVGIcon 
         imageUrl={ this.props.imageUrl } 
         customStyle={ vMenuItemIconStyle } />
