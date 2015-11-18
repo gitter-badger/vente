95602a71ab63fdff995f6b7effb37d2f27c94cb5
nvan <n.vanalder@gmail.com>
2015-11-18 20:47:56 +0100

    Update menu + add UpdateClient file
    Modified menu + created UpdateClient.js file + UpdateClient route

--- src/utilityComponents/OffScreenNavigationMenu.js
+++ src/utilityComponents/OffScreenNavigationMenu.js
@@ -9,31 +9,35 @@ import MenuItem from './MenuItem'
 * @author Mayas Haddad
 */
 export default class OffScreenNavigationMenu extends React.Component {
-	
+
 	constructor(props) {
 
 		super(props)
 		this.props = props
-		
+
 	}
 
 	render() {
 		let vAnimationName = this.props.menuState === 'out' ? 'menuout' : 'menuin'
-		
+
 		const vListElementStyle = {color: 'white', textDecoration: 'none'}
-		
+
+		const Dashboard = <li><a style={vListElementStyle} href="#/" >Dashboard </a></li>
+		const CreateNewLead = <li><a style={vListElementStyle} href="#/create-new-lead" >Create Lead </a></li>
+		const UpdateLead = <li><a style={vListElementStyle} href="#/update-lead" >Update Lead</a></li>
+		const UpdateClient = <li><a style={vListElementStyle} href="#/update-client" >Update Client</a></li>
 		const ItemOne = <MenuItem menuItemTitle="DASHBOARD" imageUrl="./src/icons/dashboard.svg" />
 		const ItemTwo = <MenuItem menuItemTitle="SETTINGS" imageUrl="./src/icons/settings.svg" />
 
-		const Items = [ItemOne, ItemTwo]
+		const Items = [Dashboard, CreateNewLead, UpdateLead, UpdateClient, ItemOne, ItemTwo]
 		if(this.props.menuState !== 'none') {
 			return (
-				<div 
+				<div
 					className="menu"
-					style={{ 
-						float: 'left', 
-						position: 'fixed', 
-						height: 100 + '%', 
+					style={{
+						float: 'left',
+						position: 'fixed',
+						height: 100 + '%',
 						backgroundColor: ColorsTheme[this.props.theme].menuBackground,
 						animation: vAnimationName + ' ' + 1 + 's',
 						width: 20 + 'em',
@@ -41,15 +45,12 @@ export default class OffScreenNavigationMenu extends React.Component {
 						}}>
 					WELCOME TO YOUR VENTE ACCOUNT
 					<ul>
-						<li><a style={vListElementStyle} href="#/" >Dashbord </a></li>
-						<li><a style={vListElementStyle} href="#/create-new-lead" >Create Lead </a></li>
-						<li><a style={vListElementStyle} href="#/update-lead" >Update Lead</a></li>
-        			</ul>
         			<Menu menuItems={Items} />
-	        	</div>
-	        )
+					</ul>
+    		</div>
+	    )
 		}
-		
+
 		return <div></div>
 	}
 }
